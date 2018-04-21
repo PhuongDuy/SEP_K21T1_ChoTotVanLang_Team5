5ae111ad3a06fb951f3263a0a0b1fbfbdaa32037
PhuongDuy <bixoan2101@gmail.com>
2018-04-21 22:38:17 +0700

    Add view List Of Product

--- index.js
+++ index.js
@@ -19,18 +19,26 @@ var promsie = mongoose.connect('mongodb://DuyHo:hothanhphuongduy@ds135179.mlab.c
 
 app.set('view engine','ejs');
 //set views path
-app.set('views',path.join(__dirname,'app/views'));
+app.set('views',path.join(  __dirname,'/app/views/backend/view'));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app/public'));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
-
+// app.set('public',path.join(__dirname, '/app/public'));
 
 //content
+// app.get('/Home',function(req,res){
+//     res.render('Home');
+// });
+// var store = require('store');
+app.get('/ListOfPost',function(req,res){
+    res.render('ListOfPost');
+});
 
 
-var port = process.env.port || 3001;
+
+var port = process.env.port || 3000;
 
 app.listen(port, function () {
     console.log('Server star in localhost:' + port);
